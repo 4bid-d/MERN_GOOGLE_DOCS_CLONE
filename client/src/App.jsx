ae297af88b33dfcd310ac844a794996a5ffd1ff5
4bid-d <abidpponline@gmail.com>
2022-09-07 15:21:25 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import TextEditor from "./TextEditor";
+import "./style.css"
+
+  function App() {
+    return (
+        <>
+      <TextEditor/>       
+        </>
+    );
+  }
+
+  export default App;
